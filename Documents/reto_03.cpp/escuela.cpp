69169f8ceb2e10219c958aa23cef491eb134cd74
xavier <armandoxaviermaganacouoh@gmail.com>
2026-04-23 23:08:44 -0600

    Agrego reto_03.cpp en la rama reto_03

---
+++ Documents/reto_03.cpp/escuela.cpp
@@ -0,0 +1,64 @@
+
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Persona{
+    private:
+        string nombre;
+        int edad;
+    public:
+        Persona(string,int);
+        virtual void mostrar();   
+};
+
+class Alumno : public Persona{
+    private:
+        float notaFinal;
+    public:
+        Alumno(string,int,float);
+        void mostrar();
+};
+
+class Profesor : public Persona {
+    private : 
+        string materia;
+    public:
+        Profesor (string, int , string);
+        void mostrar();
+};
+
+Persona::Persona(string _nombre,int _edad){
+    nombre = _nombre;
+    edad = _edad;   
+}
+
+void Persona ::mostrar(){
+    cout<<"Nombre: "<<nombre<<endl;   
+    cout<<"Edad: "<<edad<<endl;
+}
+
+Alumno::Alumno(string _nombre,int _edad,float _notaFinal) : Persona(_nombre,_edad){
+    notaFinal = _notaFinal;
+}
+
+void Alumno::mostrar(){
+    Persona::mostrar();
+    cout<<"notaFinal: "<<notaFinal<<endl;
+}
+
+Profesor::Profesor(string _nombre, int _edad,string _materia) : Persona(_nombre, _edad) {
+    materia= _materia;
+}
+
+void Profesor::mostrar(){
+    Persona::mostrar();
+    cout<<"Materia: "<<materia<<endl;
+}
+
+int main (){
+
+    system("pause");
+    return 0;
+}
